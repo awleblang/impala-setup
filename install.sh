8e763c61e894076ab9a50f8a1dd58e9c4180e802
Dimitris Tsirogiannis <dtsirogiannis@cloudera.com>
2015-04-18 15:27:21 -0700

    Improvements in install.sh script

--- install.sh
+++ install.sh
@@ -4,18 +4,31 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-# This runs as root on the server
+
+if [ $USER != "root" ]; then
+  echo "install.sh must be run as root (sudo ./install.sh)"
+  exit 1
+fi
+
+USER=$SUDO_USER
+USER_HOME=$(eval echo ~${SUDO_USER})
 
 chef_binary=/usr/bin/chef-solo
-
 # Are we on a vanilla system?
 if ! test -f "$chef_binary"; then
   export DEBIAN_FRONTEND=noninteractive
+  # Install Chef
   curl -L https://www.opscode.com/chef/install.sh | bash
 fi
 
-git clone
+cd $USER_HOME
+if ! test -d impala-setup; then
+  sudo -u $USER git clone http://github.mtv.cloudera.com/dtsirogiannis/impala-setup.git
+  cd impala-setup
+else
+  cd impala-setup
+  sudo -u $USER git pull
+fi
 
-echo "cookbook_path [ '${PWD}/cookbooks' ]" > .chef/knife.rb
-
+sudo -u $USER sed -i "s/username = ''/username = '$USER'/" cookbooks/impala/attributes/default.rb
 "$chef_binary" -c solo.rb -j impala.json
