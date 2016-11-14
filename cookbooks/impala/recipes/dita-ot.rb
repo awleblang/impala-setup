660648a9d64d2977f1777ff24e686bd2bb85f3cf
Jim Apple <jbapple@cloudera.com>
2016-11-14 10:11:04 -0800

    Add install script for dita-ot, the tool used to build Impala's docs.
    While I'm here, make install.sh executable.

---
+++ cookbooks/impala/recipes/dita-ot.rb
@@ -0,0 +1,19 @@
+# Cookbook Name:: impala
+# Recipe:: dita-ot
+#
+# Copyright 2016, Cloudera Inc.
+
+
+bash 'install_dita-ot' do
+  user node['impala_dev']['username']
+  code <<-EOF
+  UNZIP_LOCATION=$(mktemp -d)
+  cd "${UNZIP_LOCATION}"
+  wget https://github.com/dita-ot/dita-ot/releases/download/2.3.3/dita-ot-2.3.3.zip
+  unzip dita-ot-2.3.3.zip
+  sudo cp -r dita-ot-2.3.3 /opt/
+  cd ~
+  rm -rf "${UNZIP_LOCATION}"
+  echo 'PATH=/opt/dita-ot-2.3.3/bin:"${PATH}"' >> ~/.bashrc
+  EOF
+end
