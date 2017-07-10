70c385f13e861588c2d5cb23decc02dbe909e13b
David Knupp <dknupp@cloudera.com>
2017-07-10 10:13:32 -0700

    Update Impala cookbook attributes to install Oracle 8 instead of 7

--- cookbooks/impala/attributes/default.rb
+++ cookbooks/impala/attributes/default.rb
@@ -11,7 +11,7 @@ default['impala_dev']['username'] = username
 
 # Java options
 default['java']['install_flavor'] = 'oracle'
-default['java']['jdk_version'] = '7'
+default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
 # Postgres options
